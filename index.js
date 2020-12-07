0758fb851cee81ddd0d22e348a0f6142f13a233a
Aga K <59511312+agnieszkaq@users.noreply.github.com>
2020-12-07 09:49:35 +0100

    Add files

---
+++ index.js
@@ -0,0 +1,17 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/sw.js');
+}
+
+
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    addBtn.style.display = 'block';
+
+});
+
